844743ee51da8c27c5fcb34f293808056bd459c7
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-03-17 15:30:22 +0300

    HW3 is done

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, {ChangeEvent, KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    onKeyPressHandler: (e: KeyboardEvent<HTMLInputElement>) => void
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: () => void // need to fix any
+    error: string | null // need to fix any
+    totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+    {name, onKeyPressHandler, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
     const inputClass = s.error // need to fix with (?:)
 
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+        <div className={s.container}>
+            <div className={s.content}>
+                <div className={s.addItem}>
+                    <input value={name}
+                           onChange={setNameCallback}
+                           onKeyPress={onKeyPressHandler}
+                           className={s.textField}
+                    />
+                    <button className={s.btn} onClick={addUser}>+</button>
+                </div>
+                <div className={s.countUsers}>{totalUsers}</div>
+                {error && <div className={s.error}>{error}</div>}
+            </div>
         </div>
     )
 }
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
+import {UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType> // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
 }
 
 // более простой и понятный для новичков
@@ -12,21 +13,39 @@ type GreetingContainerPropsType = {
 // более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string | null>(null) // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (error !== null) {
+            setError(null)
+            addUser()
+        }
+        if (e.key === "Enter") {
+            addUser()
+        }
+    }
+
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        setName(e.currentTarget.value) // need to fix
     }
     const addUser = () => {
-        alert(`Hello  !`) // need to fix
+        if (name.trim() !== "") {
+            addUserCallback(name.trim())
+            alert(`Hello ${name.trim()} !`)
+            setName("")
+        } else {
+            setError("You trying to add an empty text")
+        }
+        // need to fix
     }
 
-    const totalUsers = 0 // need to fix
+    const totalUsers = users.length // need to fix
 
     return (
         <Greeting
             name={name}
+            onKeyPressHandler={onKeyPressHandler}
             setNameCallback={setNameCallback}
             addUser={addUser}
             error={error}
