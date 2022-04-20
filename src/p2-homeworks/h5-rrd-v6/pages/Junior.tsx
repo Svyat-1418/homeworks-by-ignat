37f0fc3056759a2e63d1de1e1fbd3c745c10eb79
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-04-20 22:32:38 +0300

    HW7 is done

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import {Navigation, Pagination, Parallax} from "swiper";
+import {Swiper, SwiperSlide} from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import {Title} from "../../../p1-main/m1-ui/u1-app/Title";
+import {Subtitle} from "../../../p1-main/m1-ui/u1-app/Subtitle";
+import HW7 from '../../h7/HW7';
 
 export function Junior() {
     return (
         <div>
+            <Swiper
+                speed={600}
+                parallax={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Parallax, Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <div
+                    slot="container-start"
+                    className="parallax-bg"
+                    data-swiper-parallax="-23%"
+                >
+                </div>
+                <SwiperSlide>
+                    <Title title={"HW7"}/>
+                    <Subtitle subtitle={"SuperSelect & SuperRadio Components"}/>
+                    <HW7/>
+                </SwiperSlide>
+            </Swiper>
+
             {/*<HW7/>*/}
             {/*<HW8/>*/}
             {/*<HW9/>*/}
--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, InputHTMLAttributes, DetailedHTMLProps} from 'react'
+import s from "./SuperRadio.module.css"
 
 type DefaultRadioPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
@@ -16,17 +17,25 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // onChange, onChangeOption
+        onChange && onChange(e)
+
+        onChangeOption &&  onChangeOption(e.currentTarget.value)
     }
 
+    const fullRadioClassNave = `${s.radio}`
 
-    const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
-        <label key={name + '-' + i}>
+    const mappedOptions: any[] = options ? options.map((option, index) => ( // map options with key
+        <label key={name + '-' + index}>
             <input
                 type={'radio'}
+                name={name}
+                checked={option === value}
+                onChange={onChangeCallback}
+                value={option}
+                className={fullRadioClassNave}
                 // name, checked, value, onChange
             />
-            {o}
+            {option}
         </label>
     )) : []
 
